c02ff972262ff5366867b3922d10e6d591e85ce5
Norman Chan <slowmanchan@gmail.com>
2016-05-03 17:14:48 -0400

    finished adding commenting functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -43,5 +43,7 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
 end
